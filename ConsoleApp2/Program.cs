d432509e293ef3bec7d97b3492ccbaf2e97dc0a1
Mateusz Kowalski <mateusz.karol.kowalski@gmail.com>
2018-07-04 16:36:32 +0200

    Mariusz naucz się czytać changes

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
@@ -10,6 +6,14 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            string name = args[0];
+            Console.WriteLine("Witaj, " + name);
+            //foreach (var item in args)
+            //{
+            //    Console.WriteLine("Witaj, " + item);
+            //}
+           
+            
         }
     }
 }
